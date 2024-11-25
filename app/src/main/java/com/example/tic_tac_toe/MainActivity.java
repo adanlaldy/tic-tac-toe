48a5d61bf2bd1e6c038f3a5fdf6e70f58bb74b5f
adanlaldy1 <adanlaldy@gmail.com>
2024-11-25 21:28:51 +0100

    add: clear method and back to lobbies page redirection

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     Button buttonStart;
     TextView scorePlayerA;
     TextView scorePlayerB;
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +33,51 @@ public class MainActivity extends AppCompatActivity {
         scorePlayerA = findViewById(R.id.txt_player_a_score);
         scorePlayerB = findViewById(R.id.txt_player_b_score);
 
-        // button to start a game
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startGame();
-            }
-        });
+        // init prefs and the editor
+        prefs = getSharedPreferences("game_prefs", MODE_PRIVATE);
+        editor = prefs.edit();
 
+        scoreManagement();
+    }
+
+    public void startGame(View view) {
+        Intent startGame = new Intent(getApplicationContext(), GameActivity.class);
+        startGame.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(startGame);
+    }
+
+    public void scoreManagement() {
         // init scores for players
-        SharedPreferences prefs = getSharedPreferences("game_prefs", MODE_PRIVATE);
         int scoreA = prefs.getInt("score_playerA", 0);
         int scoreB = prefs.getInt("score_playerB", 0);
 
-        // set scores
+        // set scores in the textViews
         scorePlayerA.setText(String.valueOf(scoreA));
         scorePlayerB.setText(String.valueOf(scoreB));
     }
 
-    public void startGame() {
-        Intent startGame = new Intent(getApplicationContext(), GameActivity.class);
-        startActivity(startGame);
+    public void clearScore(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Do you really want to clear the scores?")
+                .setTitle("Warning!")
+                .setPositiveButton("Yes", (dialog, which) -> {
+                    // clear scores in prefs
+                    editor.putInt("score_playerA", 0);
+                    editor.putInt("score_playerB", 0);
+                    editor.apply();
+
+                    // clear scores in the textViews
+                    scorePlayerA.setText("0");
+                    scorePlayerB.setText("0");
+                    dialog.dismiss();
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                .show();
+    }
+
+    public void goToLobbiesPage(View view) {
+        Intent lobbies = new Intent(getApplicationContext(), LobbyActivity.class);
+        lobbies.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(lobbies);
     }
 }
