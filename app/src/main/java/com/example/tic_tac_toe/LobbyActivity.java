c5133a07c2c38fd9c4f8aa0e8e8df855ba5c92dd
adanlaldy1 <adanlaldy@gmail.com>
2024-11-25 16:19:32 +0100

    add: simple redirections

--- app/src/main/java/com/example/tic_tac_toe/LobbyActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/LobbyActivity.java
@@ -1,6 +1,9 @@
 package com.example.tic_tac_toe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LobbyActivity extends AppCompatActivity {
 
+    Button buttonServer1;
+    Button buttonServer2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lobby);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        buttonServer1 = findViewById(R.id.btn_server_1);
+        buttonServer2 = findViewById(R.id.btn_server_2);
+
+
+        buttonServer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToServer1();
+            }
         });
+
+        buttonServer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToServer2();
+            }
+        });
+    }
+
+    public void goToServer1() {
+        Intent server1 = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(server1);
+    }
+
+    public void goToServer2() {
+        Intent server2 = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(server2);
     }
 }
